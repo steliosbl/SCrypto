0d4ec41b83371b3001deaf461e8e03ff1d369b2c
Sty BL <stysdaman@gmail.com>
2016-09-06 09:37:30 +0300

    Added copyright headers to all files.

--- SCrypto/SCrypto.PGP.SPGP.cs
+++ SCrypto/SCrypto.PGP.SPGP.cs
@@ -1,4 +1,33 @@
-﻿namespace SCrypto.PGP
+﻿#region Copyright
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="SCrypto.PGP.SPGP.cs">
+//
+// Copyright (C) 2016 Stelio Logothetis
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the +terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/. 
+// </copyright>
+// <summary>
+// SCrypto cryptographic function library for C#.
+// Email: stel.logothetis@gmail.com
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+#endregion
+
+/// <summary>
+/// Collection of classes and methods pertaining to the PGP cryptographic standard.
+/// </summary>
+namespace SCrypto.PGP
 {
     using System;
     using System.IO;
